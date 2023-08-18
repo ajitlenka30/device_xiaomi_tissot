31125a104cb016e2409c2aadd6ab4c68380127b3
ajitlenka30 <ajitlenka30@gmail.com>
2023-08-18 12:15:47 +0530

    tissot: rising os bring up

--- lineage_tissot.mk
+++ lineage_tissot.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common Rising OS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from tissot device
@@ -31,6 +31,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
 
-# LineageOS Stuffs
+# Rising OS stuff
+RISING_CHIPSET := Snapdragon-625
+RISING_MAINTAINER := Ajit
+TARGET_ENABLE_BLUR := false
+TARGET_HAS_UDFPS := false
+TARGET_USE_GOOGLE_TELEPHONY := false
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_QUICK_TAP := true
---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/ajitlenka30/device_xiaomi_msm8953-common.git device/xiaomi/msm8953-common
+git clone https://github.com/Lafactorial/vendor_xiaomi_tissot.git -b thirteen vendor/xiaomi/tissot
+git clone https://github.com/Lafactorial/vendor_xiaomi_msm8953-common -b lineage-20.0 vendor/xiaomi/tissot-common
+git clone https://github.com/Anothermi1/Another_Kernel_tissot-4.9 -b NON-OC kernel/xiaomi/msm8953
